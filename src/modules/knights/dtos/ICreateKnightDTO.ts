f6126fc97278b1e472cd96e28779c4d0e7bb0ea9
David Augusto <davidribeiro086@gmail.com>
2024-05-01 21:28:30 -0300

    feat: Create DTOs for Knight

---
+++ src/modules/knights/dtos/ICreateKnightDTO.ts
@@ -0,0 +1,13 @@
+import { Attribute } from "../infra/typeorm/entities/Attribute";
+import { Weapon } from "../infra/typeorm/entities/Weapon";
+
+interface ICreateKnightDTO {
+    name: string;
+    nickname: string;
+    birthday: Date;
+    attributes: Attribute;
+    keyAttribute: string;
+    weapons: Weapon[];
+}
+
+export { ICreateKnightDTO };
