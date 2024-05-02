054c5d46b7b097eca044985abb16772516b5a17b
David Augusto <davidribeiro086@gmail.com>
2024-05-02 11:06:10 -0300

    feat: configure list specific knight route

--- src/shared/infra/http/routes/knights.routes.ts
+++ src/shared/infra/http/routes/knights.routes.ts
@@ -1,6 +1,7 @@
 import { CreateKnightController } from "@modules/knights/useCases/CreateKnight/CreateKnightController";
 import { DeleteKnightController } from "@modules/knights/useCases/DeleteKnight/DeleteKnightController";
 import { ListKnightController } from "@modules/knights/useCases/ListKnight/ListKnightController";
+import { ListSpecificKnightController } from "@modules/knights/useCases/ListSpecificKnight/ListSpecificKnightController";
 import { UpdateKnightController } from "@modules/knights/useCases/UpdateKnight/UpdateKnightController";
 import { Router } from "express";
 
@@ -10,10 +11,11 @@ const createKnightController = new CreateKnightController();
 const listKnightController = new ListKnightController();
 const deleteKnightController = new DeleteKnightController();
 const updateKnightController = new UpdateKnightController();
+const listSpecificKnightController = new ListSpecificKnightController();
 
 knightsRoutes.get("/", listKnightController.handle);
 
-knightsRoutes.get("/:id");
+knightsRoutes.get("/:id", listSpecificKnightController.handle);
 
 knightsRoutes.post("/", createKnightController.handle);
 
