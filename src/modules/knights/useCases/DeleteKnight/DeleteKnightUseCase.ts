486823b5b0ec3f0ad6f1ebcbc161872d66cef5a5
David Augusto <davidribeiro086@gmail.com>
2024-05-02 11:05:07 -0300

    fix: Corrected imports

--- src/modules/knights/useCases/DeleteKnight/DeleteKnightUseCase.ts
+++ src/modules/knights/useCases/DeleteKnight/DeleteKnightUseCase.ts
@@ -3,6 +3,8 @@ import { Knight } from "@modules/knights/infra/typeorm/entities/Knight";
 import { IKnightsRepository } from "@modules/knights/repositories/IKnightsRepository";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "@shared/infra/http/errors/AppError";
+
 interface IRequest {
     id: string;
 }
@@ -16,6 +18,10 @@ class DeleteKnightUseCase {
 
     async execute({ id }: IRequest): Promise<Knight | undefined> {
         const knight = await this.knightsRepository.delete(id);
+
+        if (!knight) {
+            throw new AppError("Herói não encontrado", 404);
+        }
 
         return knight;
     }
